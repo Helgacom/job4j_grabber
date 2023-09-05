f84ecff09dbfb8ec8f16902bf395c2a79ede125d
Olga Remneva <remneva_olga@mail.ru>
2023-09-05 13:36:46 +0400

    quartz - AlertRabbit

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,53 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+
+    public static void main(String[] args) {
+        try {
+            String path = ".src/main/resources/rabbit.properties";
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(
+                            Integer.parseInt(getProperties(path).getProperty("rabbit.interval"))
+                    )
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    public static Properties getProperties(String path) {
+        Properties properties = new Properties();
+        try (FileInputStream in = new FileInputStream(path)) {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
+    }
+}
